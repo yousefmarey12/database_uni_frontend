da48301bf601d80a49cfad7631cd6600a017df2c
yousefmarey12 <yousef.marey@ucalgary.ca>
2025-11-07 18:36:33 +0300

    feat(src): build the initial prototype for the app

---
+++ src/auth/signup.tsx
@@ -0,0 +1,81 @@
+
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { useEffect, useRef } from "react";
+
+export function Signup({ setCurrentUser }) {
+    let formRef = useRef<HTMLFormElement>(null)
+    console.log(import.meta.env.VITE_apiKey)
+    const firebaseConfig = {
+        apiKey: import.meta.env.VITE_apiKey,
+        authDomain: "fueproject-edf68.firebaseapp.com",
+        databaseURL: "https://fueproject-edf68-default-rtdb.firebaseio.com",
+        projectId: "fueproject-edf68",
+        storageBucket: "fueproject-edf68.firebasestorage.app",
+        messagingSenderId: "763646629469",
+        appId: "1:763646629469:web:bbef95c456019500529fb4"
+    };
+
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    let auth = getAuth(app)
+    let func = e => {
+
+        console.log("hello ss")
+
+        let formData = new FormData(formRef.current as HTMLFormElement)
+        let obj = Object.create(null)
+        formData.forEach((v, k) => {
+            obj[k] = v
+        })
+        fetch('http://localhost:5000/signup', {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(obj)
+        })
+            .then((v) => {
+                return v.json()
+            })
+            .then((v) => {
+
+
+
+                signInWithEmailAndPassword(auth, obj.signup_email, obj.signup_password)
+                    .then((v) => {
+                        console.log("Logged in")
+                        let obj1 = {
+                            email: v.user.email,
+                            uid: v.user.uid
+                        }
+                        setCurrentUser(obj1)
+
+                    })
+            })
+            .catch((e) => {
+                console.log("ERR")
+                console.log(e)
+            })
+    }
+    return (
+        <form ref={formRef} onSubmit={e => {
+            e.preventDefault()
+
+        }
+        } id="form_signup">
+            <div id="signup">
+                <h1>Sign Up</h1>
+                <label htmlFor="signup_email">Email</label>
+                <input id="signup_email" name="signup_email" placeholder="Enter Email" type="email" />
+                <label htmlFor="signup_password">Password</label>
+                <input id="signup_password" name="signup_password" placeholder="Enter Password" type="password" />
+
+            </div>
+
+            <button onClick={func} type="submit">Submit</button>
+        </form>
+    )
+
+
+}
---
+++ src/chats/oldChats/oldChat.jsx
@@ -0,0 +1,101 @@
+import { useEffect, useState, useRef } from "react"
+import { getChatID } from "../../helper"
+
+export function OldChat({ currentUser, otherPerson }) {
+    let [isShowed, setIsShowed] = useState(false)
+    let [chats, setChats] = useState([])
+    let [OtherUser, setOtherUser] = useState(null)
+    let inputRef = useRef(null)
+    let chatID = getChatID(currentUser.uid, otherPerson)
+    useEffect(() => {
+        fetch('http://localhost:5000/users/' + otherPerson, {
+            headers: {
+                "Content-Type": 'application/json'
+            }
+        }).then((v) => {
+            return v.json()
+        })
+            .then((v) => {
+                console.log("v")
+                console.log(v)
+                setOtherUser(v)
+            })
+
+            .then((v) => {
+                fetchChat(chatID)
+            })
+            .catch((e) => {
+                console.log("error in user")
+                console.log(e)
+            })
+    }, [isShowed])
+
+
+    let fetchChat = id => {
+        setInterval(() => {
+            fetch('http://localhost:5000/chat/' + id, {
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            })
+                .then((v) => {
+                    return v.json()
+                })
+                .then(v => {
+                    console.log("v chats")
+                    if (Array.isArray(v)) {
+                        console.log(v)
+                        setChats(v)
+                    }
+                    else {
+                        setChats([v])
+                    }
+
+                })
+        }, 1000)
+
+    }
+
+    let func = () => {
+        let obj = {
+            message: inputRef.current.value,
+            email: currentUser.email,
+            id: chatID
+        }
+        fetch('http://localhost:5000/chat', {
+            method: 'POST',
+            headers: {
+                "Content-Type": 'application/json'
+            },
+            body: JSON.stringify(obj)
+        })
+            .then((v) => {
+                return v.json()
+            })
+            .then((v) => {
+                console.log("v chats")
+                console.log(v)
+                setChats(v)
+            })
+            .catch((e) => {
+                console.log("Error in chat")
+                console.log(e)
+            })
+
+    }
+
+
+    return (<>
+        <div onClick={() => setIsShowed(!isShowed)}>Chat with {OtherUser ? OtherUser.username : ''}</div>
+        <ul style={{ display: isShowed ? 'block' : 'none' }}>
+            {chats.length != 0 ? chats.map((el, i) => el.message && el.email ? (
+                <li key={i}>{el.email}: {el.message}</li>
+            ) : '') : ''}
+        </ul>
+        <input type="text" ref={inputRef} name="input" placeholder="Enter Chat" />
+        <button type="button" onClick={func}>Send</button>
+
+
+    </>)
+
+}
